0dec5591235a3b29a2e02bc831ae9100eec0aae4
LemillionX <tiana.rasamimanana.2019@polytechnique.org>
2023-04-18 22:15:25 +0200

    third example

--- src/training.py
+++ src/training.py
@@ -24,7 +24,7 @@ FLUID_SETTINGS["source"] = None
 
 # Load data from .json file
 CONSTRAINT = {}
-CONSTRAINT_FILE = "batch1_traj1"
+CONSTRAINT_FILE = "batch1_traj3"
 with open("../data/"+CONSTRAINT_FILE+".json") as file:
     print('Loading file', CONSTRAINT_FILE+".json")
     CONSTRAINT = json.load(file)
